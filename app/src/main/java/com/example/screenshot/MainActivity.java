8485a42b4c496f9b3c4682d0bf4a54d74a4c92a3
xinrui <guoxinrui_qd@caxins.com>
2019-02-20 17:42:38 +0800

    simple

--- app/src/main/java/com/example/screenshot/MainActivity.java
+++ app/src/main/java/com/example/screenshot/MainActivity.java
@@ -1,20 +1,51 @@
 package com.example.screenshot;
 
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] KEYWORDS = {
+            "screenshot", "screen_shot", "screen-shot", "screen shot",
+            "screencapture", "screen_capture", "screen-capture", "screen capture",
+            "screencap", "screen_cap", "screen-cap", "screen cap"
+    };
+
+    /**
+     * 读取媒体数据库时需要读取的列
+     */
+    private static final String[] MEDIA_PROJECTIONS = {
+            MediaStore.Images.ImageColumns.DATA,
+            MediaStore.Images.ImageColumns.DATE_TAKEN,
+    };
+
+    private HandlerThread mHandlerThread;
+    private Handler mHandler;
+    private MediaContentObserver mInternalObserver;
+    private MediaContentObserver mExternalObserver;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        init();
+    }
+
+    private void init() {
+        mHandlerThread = new HandlerThread("Screenshot_Observer");
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+        mInternalObserver = new MediaContentObserver(MediaStore.Images.Media.INTERNAL_CONTENT_URI, mHandler);
+        mExternalObserver = new MediaContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, mHandler);
+
+        getContentResolver().registerContentObserver(MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+                false, mInternalObserver);
+        getContentResolver().registerContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                false, mExternalObserver);
     }
 
     @Override
@@ -49,5 +95,70 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getContentResolver().unregisterContentObserver(mInternalObserver);
+        getContentResolver().unregisterContentObserver(mExternalObserver);
+    }
+
+    /**
+     * 媒体内容观察者(观察媒体数据库的改变)
+     */
+    private class MediaContentObserver extends ContentObserver {
+
+        private Uri mContentUri;
+
+        public MediaContentObserver(Uri contentUri, Handler handler) {
+            super(handler);
+            mContentUri = contentUri;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            handleMediaContentChange(mContentUri);
+        }
+    }
+
+    private void handleMediaContentChange(Uri contentUri) {
+        Cursor cursor = getContentResolver().query(contentUri, MEDIA_PROJECTIONS, null, null,
+                MediaStore.Images.ImageColumns.DATE_ADDED + " desc limit 1");
+        if (cursor == null) {
+            return;
+        }
+        if (!cursor.moveToFirst()) {
+            return;
+        }
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA));
+        long dataTaken = cursor.getLong(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_TAKEN));
+
+        handleMediaData(path, dataTaken);
+    }
+
+    private void handleMediaData(final String path, long dataTaken) {
+        if (checkScreenHot(path)) {//success
+            Log.i("Mango", "path:" + path + "        dataTaken:" + dataTaken);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    iv.setImageURI(Uri.fromFile(new File(path)));
+                }
+            });
+        } else {//failure
+            Log.i("Mango", "screen hot failure.");
+        }
+    }
+
+    private boolean checkScreenHot(String path) {
+        for (String keyWord :
+                KEYWORDS) {
+            if (path.toLowerCase().contains(keyWord)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
